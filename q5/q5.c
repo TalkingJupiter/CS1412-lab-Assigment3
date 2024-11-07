5ee88d4b3ef4fd513d5fbf738808e9690f71763e
Batuhan Sencer <dev.batuhansencer@gmail.com>
2024-11-07 13:33:53 -0600

    Planned the general structure of the C file

---
+++ q5/q5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    //TODO: Get a user input for requested function
+
+    //TODO: Write a switch case function to call the requested function
+}
+
+//TODO: Create Open account function
+
+//TODO: Create balance inquiry function
+
+//TODO: Create Deposit function
+
+//TODO: Create Withdrawal function
+
+//TODO: Create Close Account function
+
